5a1a7b45c29a751df471f04721b513307453b5bb
Bhoomika0229 <160121138+Bhoomika0229@users.noreply.github.com>
2025-11-30 23:51:26 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6zro^l!(_z%rkg=e&%if14%@(79of==-w-(tvtc5(p^%b_s$x=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
